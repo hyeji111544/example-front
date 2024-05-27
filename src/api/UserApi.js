b6acb050adca37bc736d8dcc41f7414c5070d89d
hyeji111544 <hyeji111544@gmail.com>
2024-05-27 17:57:28 +0900

    추가

--- src/api/UserApi.js
+++ src/api/UserApi.js
@@ -1,24 +1,24 @@
 import axios from 'axios';
 import { RootUrl } from './RootUrl';
 
-const rootURL = RootUrl() + '/user';
+export const rootURL = RootUrl() + '/user';
 
 export const registerUSer = async (data) => {
-    const response = await axios.post(`${rootURL}/register`,data,{
-        headers :{
-            "Content-Type": "multipart/form-data"
+    const response = await axios.post(`${rootURL}/register`, data, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
         },
     });
     return response;
 };
 
-export const loginAixos = async(data)=>{
+export const loginAixos = async (data) => {
     const response = await axios.post(`${rootURL}/login`, data);
 
     return response;
-}
+};
 
-export const getUserList = async(data)=>{
+export const getUserList = async (data) => {
     const response = await axios.get(`${rootURL}/list`);
     return response.data;
-}
+};
